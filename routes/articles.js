5019755b3575da975df00f052aaf8e86c43fbf91
practicingpragya17 <98680368+practicingpragya17@users.noreply.github.com>
2024-02-13 13:57:55 +0530

    done

--- routes/articles.js
+++ routes/articles.js
@@ -1,10 +1,26 @@
 const express = require('express')
-
+const Article = require('./../models/article')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('in this article')
+router.get('/new',(req, res) => {
+    res.render('articles/new', { article: new Article()})
 })
+router.put('/:id',(req, res)=>{
 
+})
+router.post('/', async(req, res) => {
+let article=new Article({
+      title:req.body.title,
+      description:req.body.description,
+      markdown:req.body.markdown
+
+})
+    try {
+        article = await article.save()
+        res.redirect(`/articles/${article.id}`)
+    } catch (e) {
+        res.render('articles/new', { article: article })
+    }
+})
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const articleRouter = require("./routes/articles")
 const mongoose = require('mongoose')
+const Article = require('./models/article')
 const app = express()
 
-app.set('view engine', 'ejs')
+mongoose.connect('mongodb://127.0.0.1:27017/Database')
 
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
 app.get('/',(req, res) => {
-   
-    res.render('index')
+    const articles =[{
+        title: 'Test Article 1',
+        createdAt:new Date(),
+        description:'Test description'
+    },
+    {
+        title: 'Test Article 2',
+        createdAt:new Date(),
+        description:'Test description'
+    }]
+    res.render('articles/index',{articles:articles})
 })
 
 app.use('/articles', articleRouter)
