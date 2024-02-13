0302c60d623acc46cb3102042e308c37811b6896
practicingpragya17 <98680368+practicingpragya17@users.noreply.github.com>
2024-02-13 16:11:12 +0530

    done

--- models/article.js
+++ models/article.js
@@ -1,4 +1,9 @@
 const mongoose=require('mongoose')
+const { marked } = require('marked')
+const slugify = require('slugify')
+const createDomPurify = require('dompurify')
+const { JSDOM } = require('jsdom')
+const dompurify = createDomPurify(new JSDOM().window)
 const articleSchema = new mongoose.Schema({
     title:{
         type: String,
@@ -14,10 +19,30 @@ const articleSchema = new mongoose.Schema({
     createdAt:{
         type: Date,
         default: Date.now
+    },
+    slug:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    sanitizedHTML:{
+        type: String,
+        required: true
     }
-   
 })
 
+articleSchema.pre('validate', function(next){
+    if(this.title){
+        this.slug = slugify(this.title,{lower:true, strict: true})
+    }
+    if(this.markdown){
+        this.sanitizedHTML= dompurify.sanitize(marked(this.markdown))
+    }
+
+    next()
+
+ 
+})
 
 
 module.exports = mongoose.model('Article',articleSchema)
--- server.js
+++ server.js
@@ -1,26 +1,18 @@
 const express = require('express')
 const articleRouter = require("./routes/articles")
-const mongoose = require('mongoose')
 const Article = require('./models/article')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 const app = express()
 
 mongoose.connect('mongodb://127.0.0.1:27017/Database')
 
-
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
-app.get('/',(req, res) => {
-    const articles =[{
-        title: 'Test Article 1',
-        createdAt:new Date(),
-        description:'Test description'
-    },
-    {
-        title: 'Test Article 2',
-        createdAt:new Date(),
-        description:'Test description'
-    }]
-    res.render('articles/index',{articles:articles})
+app.use(methodOverride('_method'))
+app.get('/', async(req, res) => {
+    const articles =await Article.find().sort({ createdAt:'desc'})
+    res.render('articles/index', { articles: articles })
 })
 
 app.use('/articles', articleRouter)
